7611a25c9f35b740a362eda80935a394863a725d
Brendan Darnell <brendan_darnell@hotmail.com>
2018-07-29 15:56:17 -0600

    added javascript to log the search term thumbnail results and a basic html form to input search terms

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 
 
-const YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/search'
+const YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/search';
 
-const API_KEY = 'AIzaSyD3nAYj0hHSAi8kbDfbNVGudRpQNF7Q2j4'
+const API_KEY = 'AIzaSyD3nAYj0hHSAi8kbDfbNVGudRpQNF7Q2j4';
 
-function
+function getDataFromApi(searchterm,callback) {
+  query = {
+    part: 'snippet',
+    key: API_KEY,
+    q: searchterm,
+  }
+
+  $.getJSON(YOUTUBE_URL,query,callback);
+
+}
+
+function renderResults(data) {
+  data.items.map(function(item,index){
+    let url = item.snippet.thumbnails.medium.url;
+    console.log(url) 
+    
+  })
+}
+
+
+
+
+function handleUserSearch(){
+  $('button').on('click keypress', function(event){
+    event.preventDefault()
+    let searchterm = $('input').val();
+    console.log(searchterm)
+    getDataFromApi(searchterm,renderResults);
+
+  })
+}
+
+$(handleUserSearch);
