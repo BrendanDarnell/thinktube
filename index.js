fe3f30e805f642af3db5dace0113f456311af07b
Brendan Darnell <brendan_darnell@hotmail.com>
2018-07-29 20:42:56 -0600

    programmed javascript to display thumbnail images of user search

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 const YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 const API_KEY = 'AIzaSyD3nAYj0hHSAi8kbDfbNVGudRpQNF7Q2j4';
@@ -11,29 +9,42 @@ function getDataFromApi(searchterm,callback) {
     q: searchterm,
   }
 
-  $.getJSON(YOUTUBE_URL,query,callback);
+  $.getJSON(YOUTUBE_URL,query,callback)
 
+}
+
+
+function showThumbnail(url) {
+  $('.thumbnail-container').append(
+    `<div class="thumbnail">
+      <img src="${url}" alt="thumbnail">
+    </div>`
+  )
 }
 
 function renderResults(data) {
   data.items.map(function(item,index){
     let url = item.snippet.thumbnails.medium.url;
-    console.log(url) 
+    console.log(url);
+    showThumbnail(url);
+    console.log(index);
     
   })
 }
 
 
-
-
 function handleUserSearch(){
   $('button').on('click keypress', function(event){
-    event.preventDefault()
+    event.preventDefault();
+    $('.thumbnail').remove();
     let searchterm = $('input').val();
-    console.log(searchterm)
+    console.log(searchterm);
     getDataFromApi(searchterm,renderResults);
+    $('input').val(' ');
 
   })
 }
 
 $(handleUserSearch);
+
+
